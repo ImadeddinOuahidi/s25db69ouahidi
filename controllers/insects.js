11559fe101e89e87c706716e872cb9a38100fc43
ImadeddinOuahidi <ouahidiimad@gmail.com>
2025-04-08 12:35:49 -0500

    Implement insect creation with POST endpoint

--- controllers/insects.js
+++ controllers/insects.js
@@ -1,23 +1,39 @@
 var Insect = require('../models/insects');
 // List of all insects
-exports.insect_list = async function(req, res) {
-    try{
-    theInsects = await Insect.find();
-    res.send(theInsects);
+exports.insect_list = async function (req, res) {
+    try {
+        theInsects = await Insect.find();
+        res.send(theInsects);
     }
-    catch(err){
-    res.status(500);
-    res.send(`{"error": ${err}}`);
-    } 
-   };
-   
+    catch (err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
+    }
+};
+
 // for a specific Insect.
 exports.insect_detail = function (req, res) {
     res.send('NOT IMPLEMENTED: Insect detail: ' + req.params.id);
 };
 // Handle Insect create on POST.
-exports.insect_create_post = function (req, res) {
-    res.send('NOT IMPLEMENTED: Insect create POST');
+exports.insect_create_post = async function (req, res) {
+    console.log(req.body)
+    let document = new Insect();
+    // We are looking for a body, since POST does not have query parameters.
+    // Even though bodies can be in many different formats, we will be picky
+    // and require that it be a json object
+    // {"name":"goat", "size":12, "lifespan":4}
+    document.name = req.body.name;
+    document.size = req.body.size;
+    document.lifespan = req.body.lifespan;
+    try {
+        let result = await document.save();
+        res.send(result);
+    }
+    catch (err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
+    }
 };
 // Handle Insect delete from on DELETE.
 exports.insect_delete = function (req, res) {
@@ -30,13 +46,13 @@ exports.insect_update_put = function (req, res) {
 
 // VIEWS
 // Handle a show all view
-exports.insect_view_all_Page = async function(req, res) {
-    try{
-    theInsects = await Insect.find();
-    res.render('insects', { title: 'Insect Search Results', results: theInsects });
+exports.insect_view_all_Page = async function (req, res) {
+    try {
+        theInsects = await Insect.find();
+        res.render('insects', { title: 'Insect Search Results', results: theInsects });
     }
-    catch(err){
-    res.status(500);
-    res.send(`{"error": ${err}}`);
-    } 
-   };
+    catch (err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
+    }
+};
