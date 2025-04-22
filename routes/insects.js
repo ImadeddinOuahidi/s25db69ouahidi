8ee31c393305771391b767d858a6b32f0f416e37
ImadeddinOuahidi <ouahidiimad@gmail.com>
2025-04-22 17:13:38 -0500

    Protect update endpoint

--- routes/insects.js
+++ routes/insects.js
@@ -1,16 +1,22 @@
 var express = require('express');
-const insect_controller= require('../controllers/insects');
+const insect_controller = require('../controllers/insects');
 var router = express.Router();
+const secured = (req, res, next) => {
+    if (req.user) {
+        return next();
+    }
+    res.redirect('/login');
+};
 
 /* GET. */
 router.get('/', insect_controller.insect_view_all_Page);
 /* GET detail insect page */
 router.get('/detail', insect_controller.insect_view_one_Page);
 /* GET create insect page */
-router.get('/create', insect_controller.insect_create_Page);
+router.get('/create', secured, insect_controller.insect_create_Page);
 /* GET update insect page */
-router.get('/update', insect_controller.insect_update_Page);
+router.get('/update', secured, insect_controller.insect_update_Page);
 /* GET delete insect page */
-router.get('/delete', insect_controller.insect_delete_Page);
+router.get('/delete', secured, insect_controller.insect_delete_Page);
 
 module.exports = router;
