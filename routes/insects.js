e635314a08c88886ba89f982ccc61c7368b25ba0
ImadeddinOuahidi <ouahidiimad@gmail.com>
2025-04-22 19:15:46 -0500

    Bonus

--- routes/insects.js
+++ routes/insects.js
@@ -13,12 +13,10 @@ router.get('/', insect_controller.insect_view_all_Page);
 /* GET detail insect page */
 router.get('/detail', insect_controller.insect_view_one_Page);
 /* GET create insect page */
-router.get('/create', insect_controller.insect_create_Page);
+router.get('/create', secured, insect_controller.insect_create_Page);
 /* GET update insect page */
 router.get('/update', secured, insect_controller.insect_update_Page);
 /* GET delete insect page */
-router.get('/delete', insect_controller.insect_delete_Page);
-
-router.post('/create', insect_controller.insect_create_post);
+router.get('/delete', secured, insect_controller.insect_delete_Page);
 
 module.exports = router;
